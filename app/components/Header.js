72c65a8cb6bae233229a9f7b4852bb6b24fdf822
Madhummitha <64522639+madhummitha@users.noreply.github.com>
2023-11-16 17:42:19 +0000

    Basic setup

---
+++ app/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "next/image";
+
+const Header = () => {
+    return (
+        <div>
+            <p>Hi</p>
+            <p>Lorum ipsum</p>
+        </div>
+    )
+}
+
+export default Header;
---
+++ app/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "next/image";
+
+const NavBar = () => {
+    return (
+        <div>
+            <p>Hi</p>
+            <p>Lorum ipsum</p>
+        </div>
+    )
+}
+
+export default NavBar;
