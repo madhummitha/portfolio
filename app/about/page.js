a44efb938bd6eca1133deefc00db9c94d5503d14
Madhummitha <64522639+madhummitha@users.noreply.github.com>
2024-04-16 17:48:06 +0000

    Revert "misc"
    This reverts commit fd76600580b4d530f53f78d2fbcaf05cfafec225.

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
 import {
   FaAddressBook,
@@ -10,14 +9,11 @@ import {
 } from "react-icons/fa6";
 import {
   SiAmazonaws,
-  SiCsharp,
   SiDocker,
   SiFigma,
-  SiGithub,
   SiGithubactions,
   SiGoogleanalytics,
   SiJavascript,
-  SiJenkins,
   SiKubernetes,
   SiMongodb,
   SiNextdotjs,
@@ -28,68 +24,9 @@ import {
 } from "react-icons/si";
 
 const About = () => {
-  // const projects = [
-  //   {
-  //     id: 1,
-  //     title: "Areen Order Hub",
-  //     description: "An eCommerce platform built with MERN Stack.",
-  //     image: "/images/Areen.png",
-  //     link: "/docs/HR CRM.pdf",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Hiring Management Syatem",
-  //     description: "An hiring management system built with MERN Stack.",
-  //     image: "/images/Hikers.png",
-  //     link: "/docs/HR CRM.pdf",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "React Chat App",
-  //     description: "A Chatting App using Firebase Authentication.",
-  //     image: "/images/ChatApp.png",
-  //     link: "https://react-chat-app-gold.vercel.app/",
-  //   },
-  //   {
-  //     id: 4,
-  //     title: "Book Management App",
-  //     description: "A Book Management App with CRUD Operations",
-  //     image: "/images/Book.png",
-  //     link: "https://book-management-app-omega.vercel.app/",
-  //   },
-  //   {
-  //     id: 5,
-  //     title: "Budget Tracker App",
-  //     description: "A Budget Tracking App with CRUD Operations",
-  //     image: "/images/Budget.png",
-  //     link: "https://budget-tracker-8cvn.vercel.app/",
-  //   },
-  //   {
-  //     id: 6,
-  //     title: "ToDo App",
-  //     description: "A Basic TODO App with CRUD Operations",
-  //     image: "/images/ToDo.png",
-  //     link: "https://webdev-projects-flni.vercel.app/",
-  //   },
-  //   {
-  //     id: 7,
-  //     title: "MTunes App",
-  //     description: "A Clone of MTunes App using JS",
-  //     image: "/images/MTunes.png",
-  //     link: "https://webdev-projects-gamma.vercel.app/",
-  //   },
-  //   {
-  //     id: 8,
-  //     title: "Social Modia Posts App",
-  //     description: "A Basic Posting App with CRUD Operations",
-  //     image: "/images/Post.png",
-  //     link: "https://webdev-projects-wxq6.vercel.app/",
-  //   },
-  // ];
-
   return (
-    <div>
-      <div className="container mx-auto p-8 font-serif font-semibold">
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="container mx-auto p-8 font-serif font-semibold flex flex-col justify-center">
         <div className="max-w-2xl mx-auto text-center mb-8 text-purple-100">
           <p className="text-3xl mt-12 mb-3 italic">Hi there!</p>
           <p>
@@ -108,7 +45,7 @@ const About = () => {
               <p className="underline underline-offset-2 italic mb-2">
                 Frameworks & Languages:
               </p>
-              <div className="grid grid-cols-3 gap-4">
+              <div className="flex flex-wrap justify-center gap-4 max-w-xl">
                 <div className="flex items-center">
                   <SiReact className="text-l text-purple-300" />
                   <span className="ml-2">React.Js</span>
@@ -137,17 +74,14 @@ const About = () => {
                   <FaDatabase className="text-l text-purple-300" />
                   <span className="ml-2">SQL</span>
                 </div>
-                {/* <div className="flex items-center">
-                  <SiCsharp className="text-l text-purple-300" />
-                  <span className="ml-2">C#</span>
-                </div> */}
               </div>
             </div>
-            <div className="text-center mt-3">
+
+            <div className="text-center mt-5">
               <p className="underline underline-offset-2 italic mb-2">
                 DevOps & Infrastructure:
               </p>
-              <div className="grid grid-cols-3 gap-4">
+              <div className="flex flex-wrap justify-center gap-4 max-w-xl">
                 <div className="flex items-center">
                   <SiDocker className="text-l text-purple-300" />
                   <span className="ml-2">Docker</span>
@@ -161,29 +95,26 @@ const About = () => {
                   <span className="ml-2">NGINX</span>
                 </div>
                 <div className="flex items-center">
-                  <SiGithub className="text-l text-purple-300" />
-                  <span className="ml-2">GIT</span>
+                  <FaGithub className="text-l text-purple-300" />
+                  <span className="ml-2">Git</span>
                 </div>
                 <div className="flex items-center">
                   <SiGithubactions className="text-l text-purple-300" />
-                  <span className="ml-2">CI/CD</span>
+                  <span className="ml-2">CI/CD Pipeline</span>
                 </div>
                 <div className="flex items-center">
                   <SiRedis className="text-l text-purple-300" />
                   <span className="ml-2">Redis</span>
                 </div>
+
                 <div className="flex items-center">
                   <SiAmazonaws className="text-l text-purple-300" />
                   <span className="ml-2">AWS</span>
                 </div>
-                {/* <div className="flex items-center">
-                  <SiCsharp className="text-l text-purple-300" />
-                  <span className="ml-2">C#</span>
-                </div> */}
               </div>
             </div>
 
-            <div className="text-center mt-3">
+            <div className="text-center mt-5">
               <p className="underline underline-offset-2 italic mb-2">
                 Others:
               </p>
@@ -216,7 +147,6 @@ const About = () => {
           <a
             href="/docs/Madhummitha R - Resume.pdf"
             target="_blank"
-            // download="Madhummithas_Resume.pdf"
             className="bg-pink-900 text-white p-2 rounded-md flex items-center"
           >
             <FaFileArrowDown
@@ -227,36 +157,6 @@ const About = () => {
             Resume
           </a>
         </div>
-        {/* <div className="flex items-center justify-center">
-          <hr className="w-2/3  border-gray-800" />
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 bg-gradient-to-r from-purple-400 to-pink-800 bg-clip-text text-transparent mt-20">
-          {projects.map((project) => (
-            <div key={project.id} className="mb-8">
-              <a href={project.link} target="_blank" rel="noopener noreferrer">
-                <Image
-                  width={250}
-                  height={250}
-                  src={project.image}
-                  alt={project.title}
-                  className="mx-auto rounded-md"
-                />
-                <h3 className="text-lg font-semibold mb-2 text-center">
-                  {project.title}
-                </h3>
-                <p className="text-sm text-center">{project.description}</p>
-              </a>
-            </div>
-          ))}
-        </div>
-        <div className="flex justify-center">
-          <Link
-            href="/projects"
-            className="w-1/4 text-center p-2 rounded-md text-md font-semibold font-serif bg-gradient-to-r from-purple-700 to-pink-800 hover:from-pink-700 hover:to-purple-800 text-white"
-          >
-            {"Read More ➜"}
-          </Link>
-        </div> */}
       </div>
     </div>
   );
